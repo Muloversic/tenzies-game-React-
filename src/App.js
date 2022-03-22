96098189a3152f5bc19ad27f56fa7a85ab28955d
Oleh Nikulin <oleh.nikulin.dev@gmail.com>
2022-03-22 11:14:30 +0200

    created markup and style for body and die

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import './App.css';
-
+import Die from './components/Die';
 function App() {
   return (
     <main className="main">
-
+        <div className='main__body'>
+          <Die value={1} />
+          <Die value={1} />
+          <Die value={1} />
+          <Die value={1} />
+          <Die value={1} />
+          <Die value={1} />
+          <Die value={1} />
+          <Die value={1} />
+          <Die value={1} />
+          <Die value={1} />
+        </div>
     </main>
   );
 }
---
+++ src/components/Die.js
@@ -0,0 +1,7 @@
+export default function Die(props){
+    return(
+        <div className="main__die die">
+            <span>{props.value}</span>
+        </div>
+    )
+}
