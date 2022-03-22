f9976f57adcf03872cf8b711555dbd58959c2455
Oleh Nikulin <oleh.nikulin.dev@gmail.com>
2022-03-22 11:41:47 +0200

    created dice generator func and button to generate it

--- src/App.js
+++ src/App.js
@@ -1,20 +1,30 @@
 import './App.css';
 import Die from './components/Die';
+import React from 'react'
+
 function App() {
-  return (
+  function allNewDice(){
+    const randNums = []
+    for(let i = 0; i < 10; i++){
+      let rand = Math.floor( 1 + Math.random() * (6 + 1 - 1));
+      randNums.push(rand)
+    }
+
+    return randNums
+  }
+
+  function rollDice(){
+    setDice(allNewDice())
+  }
+
+  const [dice, setDice] = React.useState(allNewDice())
+  const die = dice.map(num => <Die value={num} />)
+  return ( 
     <main className="main">
         <div className='main__body'>
-          <Die value={1} />
-          <Die value={1} />
-          <Die value={1} />
-          <Die value={1} />
-          <Die value={1} />
-          <Die value={1} />
-          <Die value={1} />
-          <Die value={1} />
-          <Die value={1} />
-          <Die value={1} />
+          {die}
         </div>
+        <button className='main__button' onClick={rollDice}>Roll</button>
     </main>
   );
 }
